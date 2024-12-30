31fbde7b2c402facc09ebef460f1fafd82c4b41c
RAWANABUHAMMAD <rawan.abuhammad89@gmail.com>
2024-12-30 22:35:46 +0300

    initail

---
+++ JavaScript IF/task.js
@@ -0,0 +1,106 @@
+//1//
+
+let age=27;
+if(age>15){
+    document.write("You are an adult"+"<br>")
+    }
+
+//2//
+let score=45;
+if(score<=50){
+    document.write("You failed the test"+"<br>")
+}
+//3//
+let Name= "John" ;
+if( Name="John"){
+    document.write( "Hello, John"+"<br>")
+}
+
+//4//
+let day="monday"
+if (day!="Saturday" && day !="Sunday"){
+    document.write( "It's a weekday"+"<br>")
+}
+//5//
+let num=4;
+if(num%2==0){
+    document.write("The number is even" +"<br>")}
+
+//6//
+let char="a";
+if(char="letter"){
+    document.write( "It's a letter"+"<br>")
+}
+
+//7//
+let list=[1,2,3];
+if(list=" array"){
+    document.write( "It's an array" +"<br>")
+}
+
+//8//
+let x=5;
+if(x=" positive number") {
+    document.write("x is a positive number"+"<br>")
+}
+//9//
+let y=-5;
+if(y=" negative number"){
+    document.write("y is a negative number"+"<br>")
+}
+
+//10//
+let z=9;
+if(z % 3 == 0){
+    document.write( "z is a multiple of 3" +"<br>")
+}
+//11//
+let GPA ="a" 
+switch(GPA){
+    case"A":
+    case"a":
+    document.write("Excellent"+"<br>")
+    break;
+    case"B":
+    case"b":
+    document.write("Very Good"+"<br>")
+    break;
+    case"C":
+    case"c":
+    document.write("Good"+"<br>")
+    break;
+    case"F":
+    case"f":
+    document.write("failed "+"<br>")
+    break;
+    default:
+    document.write(" a proper message"+"<br>")}
+    
+//12//
+let month = "January"; 
+if (month === "December" , month === "January" || month === "February"){
+    document.write("a winter month"+"<br>" )}
+else if (month === "March" || month === "April" || month === "May") {
+    document.write("a spring month"+"<br>" )}
+ else if (month === "June" || month === "July" || month === "August"){
+     document.write("a summer month"+"<br>") }  
+  else if (month === "September" || month === "October" || month === "November"){  
+     document.write("autumn month"+"<br>")}
+  else{
+    document.write("unknown"+"<br>")
+  }  
+  //13// 
+  let password ="mySecurePass";
+  if (password.length >= 8) {
+    document.write("Your password is strong"+"<br>");}
+ else {
+    document.write("Your password is short"+"<br>");}
+
+ //14//  
+ 
+ 
+
+
+
+
+ //15// 
